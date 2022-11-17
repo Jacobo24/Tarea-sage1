e446c54b61e25922f2d64244e720fc4131ead806
Jacobo24 <114859002+Jacobo24@users.noreply.github.com>
2022-11-17 21:07:14 +0100

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[51]:
+
+
+28/(2^5).numerical_approx()
+
+
+# In[52]:
+
+
+sqrt(2).numerical_approx()
+
+
+# In[9]:
+
+
+sqrt(-9)
+
+
+# In[15]:
+
+
+log(2153693963075557766310747,3)
+
+
+# In[16]:
+
+
+e.n(digits=50)
+
+
+# In[17]:
+
+
+pi.n(digits=80)
+
+
+# In[48]:
+
+
+956//98 
+
+
+# In[68]:
+
+
+956%98
+
+
+# In[69]:
+
+
+98*9+74
+
+
+# In[60]:
+
+
+a = 234878%3
+a<0
+#si a<0 3 no divide 234878
+
+
+# In[61]:
+
+
+divisors(134)
+
+
+# In[62]:
+
+
+divisors(491)
+
+
+# In[63]:
+
+
+divisors(422)
+
+
+# In[64]:
+
+
+divisors(1002)
+
+
+# In[65]:
+
+
+#solo el 491 es primo
+
+
+# In[73]:
+
+
+(2).gcd(5)
+
+
+# In[72]:
+
+
+(2).lcm(5)
+
+
+# In[74]:
+
+
+(4).gcd(10)
+
+
+# In[75]:
+
+
+(4).lcm(10)
+
+
+# In[76]:
+
+
+(18).gcd(51)
+
+
+# In[77]:
+
+
+(18).lcm(51)
+
